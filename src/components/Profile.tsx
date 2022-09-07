2d2824caf346ab136117613c1111dd2e898acde8
Kelvin Souza Gonçalves <kelvinsouzag@gmail.com>
2022-09-07 18:06:26 -0300

    Adicionado CompletedChallenges

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile() {
             <img src="https://github.com/kekoksg.png" alt="Kelvin Souza Gonçalves" />
             <div>
                 <strong>Kelvin S. Gonçalves</strong>
-                <p>Level 1</p>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 1
+                </p>
             </div>
         </div>
     );
