c1570c3a9b779dc8dc8b8ac04c773e4bba6e21eb
Kelvin Souza Gonçalves <kelvinsouzag@gmail.com>
2022-09-11 19:53:52 -0300

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 // Tudo que será repetido no projeto inteiro fica no _app.tsx
+import { ChallengesProvider } from '../contexts/ChallengesContext'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  )
 }
 
 export default MyApp
